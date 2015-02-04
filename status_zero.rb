244139ecfde0a3150b520b7112df3f285f45e7ca
Rob Wilson <roobert@gmail.com>
2015-02-04 20:56:42 +0000

    first commit

---
+++ status_zero.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+#
+# Released under the same terms as Sensu (the MIT license); see LICENSE
+# for details
+#
+# this handler writes event data to redis that has status 0
+
+require 'rubygems' if RUBY_VERSION < '1.9.0'
+require 'sensu-handler'
+require 'redis'
+
+class GreenLight < Sensu::Handler
+  def handle
+    return unless @event['check']['status'] == 0
+
+    begin
+      host = settings['greenlight']['server']
+      port = settings['greenlight']['port']
+    rescue => e
+      puts "failed to configure revent handler: #{e}"
+    end
+
+    begin
+      redis = Redis.new(:host => host, :port => port)
+      redis.hset("events:" + @event['client']['name'], @event['check']['name'], @event)
+    rescue => e
+      puts "failed to write event to redis: #{e}"
+    end
+  end
+end
+
